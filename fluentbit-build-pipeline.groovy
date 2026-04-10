77d2b613abbd025d1cc1452339d2d507e9c359e2
Michael Frazier <jmfrzier@gmail.com>
2026-04-10 12:57:30 -0500

    fix2

--- fluentbit-build-pipeline.groovy
+++ fluentbit-build-pipeline.groovy
@@ -17,7 +17,8 @@ pipelineJob('fluentbit-build-pipeline') {
             stage('Checkout') {
               steps {
                 container('bookworm') {
-                  sh 'rm -rf fluent-bit-src && apt-get update && apt-get install git && git clone --depth 1 --branch \${FLUENTBIT_VERSION} https://github.com/fluent/fluent-bit.git fluent-bit-src'
+                  sh 'apt-get update && apt-get install -y --no-install-recommends git'
+                  sh 'rm -rf fluent-bit-src && git clone --depth 1 --branch \${FLUENTBIT_VERSION} https://github.com/fluent/fluent-bit.git fluent-bit-src'
                 }
               }
             }
